070d402c1018248ac3442d6f6431e7c132d1f973
sempeck <jarek.sepek@mediorama.pl>
2019-10-11 08:17:35 +0200

    Add randomizer and else if

--- js/script.js
+++ js/script.js
@@ -1,4 +1,13 @@
-let computerMove = 'kamień';
-let playerMove = 'papier';
+let randomNumber = Math.floor(Math.random() * 3 + 1);
 
-printMessage('Zagrałem ' + computerMove + ', a Ty ' + playerMove + '! Wygrywasz!');
+let computerMove = 'nieznany ruch';
+
+if (randomNumber == 1) {
+  computerMove = 'kamień';
+} else if (randomNumber == 2) {
+  computerMove = 'papier';
+} else if (randomNumber == 3) {
+  computerMove = 'nożyce';
+}; 
+
+console.log('Komputer wylosował: ' + computerMove);
